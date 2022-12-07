cf34374e44101afe14e2b4aee79620cf34dbf765
Adam Kania <adamw.kania@hotmail.com>
2022-12-07 11:05:01 -0600

    Updated naming convention for consumer NSG and subnetting for HUB VNET

--- palo_alto/azgwlb-hybrid/consumer.tf
+++ palo_alto/azgwlb-hybrid/consumer.tf
@@ -110,7 +110,7 @@ resource "azurerm_linux_virtual_machine" "consumer" {
   }
 }
 resource "azurerm_network_security_group" "consumer_nsg" {
-  name                = "Consumer NSG"
+  name                = "Consumer_NSG"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
